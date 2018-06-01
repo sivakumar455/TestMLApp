e335351b082ea15c60da9901ab1d1ad0ef193655
sivakumar <sivakumarpadala5@outlook.com>
2018-06-02 00:18:34 +0530

    added some code

--- main.py
+++ main.py
@@ -1,35 +1,27 @@
-from flask import Flask,render_template,request,url_for
-
-import pandas as pd
+from flask import Flask, render_template, request, url_for
 import numpy as np
-
-from sklearn.feature_extraction.text import CountVectorizer
-
-from sklearn.model_selection import train_test_split
-from sklearn import model_selection
-from sklearn.neighbors import KNeighborsClassifier
+import pickle
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-	return render_template("index.html")
+    return render_template("index.html")
 
-@app.route("/",methods=['POST'])
+
+@app.route("/", methods=['POST'])
 def predict():
-
-	ytb_model = open("TestML/knn.pkl","rb")
-	clf = joblib.load(ytb_model)
-
-	if request.method == 'POST':
-		comment = request.form['comment']
-		data = [comment]
-		vect = cv.transform(data).toarray()
-		my_prediction = clf.predict(vect)
-	return render_template('results.html',prediction = my_prediction,comment = comment)
-	
+    knn = pickle.load(open("knn.pkl", "rb"))
+    if request.method == 'POST':
+        comment = request.form['comment']
+        print comment
+        data = comment.split(',')
+        pre_req = np.asarray(data)
+        my_prediction = knn.predict([pre_req])
+        print my_prediction
+    return render_template('results.html', prediction=my_prediction, comment=comment)
 
 
 if __name__ == '__main__':
-	app.run(host="127.0.0.1",port=8080,debug=True)
-	
+    app.run(host="127.0.0.1", port=8080, debug=True)
